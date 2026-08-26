a5e0451c5ade35196a7180c93757f4e430810509
batodesrets93 <admin.eu@luccianos.net>
2026-08-26 15:23:26 -0300

    Add 'Pagos informados' link to NavAdmin

--- src/components/NavAdmin.tsx
+++ src/components/NavAdmin.tsx
@@ -7,6 +7,7 @@ export default function NavAdmin() {
     { href: "/admin/unidades", label: "Unidades" },
     { href: "/admin/cocheras-bauleras", label: "Cocheras y bauleras" },
     { href: "/admin/expensas", label: "Expensas" },
+    { href: "/admin/pagos-informados", label: "Pagos informados" },
     { href: "/admin/reservas", label: "Reservas" },
     { href: "/admin/reclamos", label: "Reclamos" },
   ];
