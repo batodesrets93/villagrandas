446b009ef932e05f520bed7537d4f1089ddabb3c
batodesrets93 <admin.eu@luccianos.net>
2026-08-26 12:00:26 -0300

    Include viewport settings in layout.tsx
    Added viewport configuration to layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,6 +13,11 @@ export const metadata: Metadata = {
     ],
     apple: "/apple-touch-icon.png",
   },
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
   themeColor: "#b4ada4",
 };
 
